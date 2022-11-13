9c110b9d8a527eff7673d8fa6a1ffc7e30b60c96
AshleshaV <reachaslesha@gmail.com>
2022-11-12 22:47:26 -0600

    GeoJson_LineStrings

---
+++ Mapping_GeoJSON_Linestrings/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoicmVhY2hhc2xlc2hhIiwiYSI6ImNsYTQ2c3BsdDA5Zzgzb21sZHkwazh2YWsifQ.LItRJpx3zR6R4L95PZFwWQ";
--- Mapping_Multiple_Points/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "AIzaSyDIA34aX0cyZIZJcWpQR9jP_xc4gJ1xLBE";
